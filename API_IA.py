8d871f8e17245023517fef3e66de1dfbc4af4fe6
mbmontuan <mbmontuan@hotmail.com>
2020-09-12 13:29:21 -0300

    Add files via upload

---
+++ API_IA.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import requests
+import json
+class apiIA():
+
+    def getCodEmpresa(self, nome):
+        url = 'http://localhost:3030/empresas'
+        r = requests.get(url)
+        retorno = ''
+        for x in r.json()["empresas"]:
+            if x["nome"] == nome:
+                retorno = x["codigo"]
+                break
+        return retorno
+
+    def getSentimento(self, codEmpresa,dataIni, dataFim):
+        url = 'http://localhost:3030/sentimento/data/' + codEmpresa
+        payload = {'dataInicio': dataIni, 'dataFim': dataFim}
+        r = requests.get(url, data=payload)
+        return r.json()["quantidade"]
+
+codEmpresa = apiIA().getCodEmpresa("Santander")   #se nçao achar a empresa na busca, chatbot deve avisar que nçao encontrou pesquisas sobre a empresa
+print("Codigo da empresa: " + codEmpresa)
+print(apiIA().getSentimento(codEmpresa ,"05-06-2020", "12-06-2020"))
